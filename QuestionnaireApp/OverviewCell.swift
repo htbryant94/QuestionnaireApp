6834b8f1953c7b83f0b1b4d4bc18ccb44a1fdd0e
Harry Bryant <harry.bryant@onthebeach.co.uk>
2019-03-22 19:29:25 +0000

    OverviewCell now correctly passes values to data model, with alert checks

--- QuestionnaireApp/OverviewCell.swift
+++ QuestionnaireApp/OverviewCell.swift
@@ -1,14 +1,16 @@
 import UIKit
 
-class OverviewCell: UICollectionViewCell {
+class OverviewCell: UICollectionViewCell, UITextFieldDelegate {
 
   @IBOutlet weak var lakeCheckBox: CheckBox!
   @IBOutlet weak var tackleShopCheckBox: CheckBox!
+  @IBOutlet weak var nameTextField: UITextField!
   
   var hasLake: Bool = false
   var hasTackleShop: Bool = false
   var hasLakeValueChanged: ((Bool) -> Void)?
   var hasTackleShopValueChanged: ((Bool) -> Void)?
+  var nameChanged: ((String?) -> Void)?
   
   override func awakeFromNib() {
     super.awakeFromNib()
@@ -20,6 +22,15 @@ class OverviewCell: UICollectionViewCell {
     tackleShopCheckBox.listener = { [weak self] value in
       self?.hasTackleShopValueChanged?(value)
     }
+    nameTextField.delegate = self
+  }
+  
+  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    nameTextField.resignFirstResponder()
+  }
+  
+  func textFieldDidEndEditing(_ textField: UITextField) {
+    nameChanged?(textField.text)
   }
   
 }
--- QuestionnaireApp/QuestionnaireViewController.swift
+++ QuestionnaireApp/QuestionnaireViewController.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-class QuestionnaireViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+class QuestionnaireViewController: UIViewController {
   
   @IBOutlet weak var collectionView: UICollectionView!
   var questionnaireData: QuestionnaireData?
@@ -15,67 +15,39 @@ class QuestionnaireViewController: UIViewController, UICollectionViewDelegate, U
     questionnaireData = emptyQuestionnaire()
   }
   
-  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section : Int) -> Int {
-    return 1
-  }
-  
-  func numberOfSections(in collectionView: UICollectionView) -> Int {
-    return 2
-  }
-  
-  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-    var cell: UICollectionViewCell!
-    if indexPath.section == 0 {
-      if let overviewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "OverviewCell", for: indexPath) as? OverviewCell {
-        overviewCell.hasLakeValueChanged = { [weak self] value in
-          print("lake is: \(value)")
-          self?.questionnaireData?.overview?.hasLake = value
-        }
-        overviewCell.hasTackleShopValueChanged = { [weak self] value in
-          print("tackle shop is: \(value)")
-          self?.questionnaireData?.overview?.hasTackleShop = value
-        }
-        cell = overviewCell
-      }
-    } else if indexPath.section == 1 {
-      cell = collectionView.dequeueReusableCell(withReuseIdentifier: "AddressCell", for: indexPath) as! AddressCell
-    }
-    return cell
-  }
-  
-  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-    if indexPath.section == 0 {
-      return CGSize(width: collectionView.frame.width, height: 200)
-    } else if indexPath.section == 1 {
-      return CGSize(width: collectionView.frame.width, height: 400)
-    }
-    return CGSize(width: collectionView.frame.width, height: 100)
-  }
-  
-  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-    return UIEdgeInsets(top: 0, left: 0, bottom: 20, right: 0)
-  }
-  
   @IBAction func doneButtonPressed(_ sender: Any) {
     guard let data = questionnaireData else { return }
-    handleEmptyDataError(data)
+    handleResponse(data)
   }
   
   private func emptyQuestionnaire() -> QuestionnaireData {
     return QuestionnaireData(overview: OverviewData())
   }
   
-  private func handleEmptyDataError(_ questionnaire: QuestionnaireData) {
+//  private func handleEmptyDataError(_ questionnaire: QuestionnaireData) {
+//    guard questionnaire.overview?.hasLake != nil,
+//          questionnaire.overview?.hasTackleShop != nil,
+//          questionnaire.overview?.name != nil else {
+//      presentAlert(success: false)
+//      return
+//    }
+//  }
+  
+  private func handleResponse(_ questionnaire: QuestionnaireData) {
     guard questionnaire.overview?.hasLake != nil,
-          questionnaire.overview?.hasTackleShop != nil else {
-      presentAlert()
+          questionnaire.overview?.hasTackleShop != nil,
+          questionnaire.overview?.name != nil else {
+      presentAlert(success: false)
       return
     }
+    presentAlert(success: true)
   }
   
-  private func presentAlert() {
-    let alert = UIAlertController(title: "There was an issue",
-                                  message: "One or more fields have been left blank",
+  private func presentAlert(success: Bool) {
+    let title = success ? "Success" : "There was an Issue"
+    let message = success ? "All fields have values" : "One or more fields have been left blank"
+    let alert = UIAlertController(title: title,
+                                  message: message,
                                   preferredStyle: .alert)
     let action = UIAlertAction(title: "Ok", style: .default) { _ in
       alert.dismiss(animated: true, completion: nil)
@@ -94,4 +66,68 @@ struct OverviewData {
   var name: String?
   var hasLake: Bool?
   var hasTackleShop: Bool?
+}
+
+// MARK: UICollectionViewDataSource
+extension QuestionnaireViewController: UICollectionViewDataSource {
+  
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section : Int) -> Int {
+    return 1
+  }
+  
+  func numberOfSections(in collectionView: UICollectionView) -> Int {
+    return 2
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    var cell: UICollectionViewCell!
+    if indexPath.section == 0 {
+      if let overviewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "OverviewCell", for: indexPath) as? OverviewCell {
+        cell = configureOverviewCell(overviewCell)
+      }
+    } else if indexPath.section == 1 {
+      cell = collectionView.dequeueReusableCell(withReuseIdentifier: "AddressCell", for: indexPath) as! AddressCell
+    }
+    return cell
+  }
+
+}
+
+// MARK: Cells
+extension QuestionnaireViewController: UICollectionViewDelegate {
+  
+  private func configureOverviewCell(_ cell: OverviewCell) -> OverviewCell {
+    cell.hasLakeValueChanged = { [weak self] value in
+      print("lake is: \(value)")
+      self?.questionnaireData?.overview?.hasLake = value
+    }
+    cell.hasTackleShopValueChanged = { [weak self] value in
+      print("tackle shop is: \(value)")
+      self?.questionnaireData?.overview?.hasTackleShop = value
+    }
+    cell.nameChanged = { [weak self] value in
+      print("name changed: \(value!)")
+      self?.questionnaireData?.overview?.name = value
+    }
+    return cell
+  }
+
+}
+
+// MARK: UICollectionViewDelegateFlowLayout
+extension QuestionnaireViewController: UICollectionViewDelegateFlowLayout {
+  
+  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+    if indexPath.section == 0 {
+      return CGSize(width: collectionView.frame.width, height: 200)
+    } else if indexPath.section == 1 {
+      return CGSize(width: collectionView.frame.width, height: 400)
+    }
+    return CGSize(width: collectionView.frame.width, height: 100)
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+    return UIEdgeInsets(top: 0, left: 0, bottom: 20, right: 0)
+  }
+  
 }
